914cefd19032372b940c9f8f81c330ac71b3074e
gauravkumar-tech <gauravkumar28051997@gmail.com>
2022-07-11 21:08:29 +0530

    json fake database added and working on functionality.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
+import user from "./pass.json";
+
+
+interface pass {  
+  id: Number;  
+  name: String;  
+  email: String;  
+  gender: String;  
+  password: String;  
+  user_role: String;    
+}  
 
 @Component({
   selector: 'app-login',
@@ -9,11 +21,30 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class LoginComponent implements OnInit {
   constructor(private spinner: NgxSpinnerService) { }
   message = "loading..."
+  users:pass[]=user;
+  loginForm!:FormGroup
+
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl(null),
+      password: new FormControl(null)
+    })
   }
 
   login(){
     this.spinner.show();
+
+    let userObj = this.users.filter(e=>e.name == this.loginForm.controls['username'].value)[0]
+    
+    if(userObj!=undefined || userObj!=null){
+      if(userObj.password == this.loginForm.controls['password'].value){
+        this.message = `welcome ${userObj.name} ...`
+        localStorage.setItem("user",JSON.stringify(userObj));
+      }else{
+
+      }
+    }
+
     setTimeout(()=>{
       this.spinner.hide();
     },2000)
